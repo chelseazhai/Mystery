4410ead2441247aa2decd44eb3fe2b607f783bc2
chelsea zhai <chelsea.zhai@gmail.com>
2013-02-05 16:08:22 +0800

    xcode project file updated

--- Mystery/views/ABContactsListView.m
+++ Mystery/views/ABContactsListView.m
@@ -239,7 +239,7 @@
             case contactModify:
             {
                 // save the modify contact index of all contacts info array in addressBook reference and new temp all contacts info array in addressBook
-                NSInteger _oldindex, _newIndex;
+                NSInteger _oldindex = 0, _newIndex = 0;
                 for (NSInteger _index = 0; _index < [_allContactsInfoArrayInABRef count]; _index++) {
                     if (((ContactBean *)[_allContactsInfoArrayInABRef objectAtIndex:_index]).id == _contactId.integerValue) {
                         _oldindex = _index;
